02edb04a096ba34c3c32acf413728ecb6a4c1892
Justicegodknows <justice.samuel@code.berlin>
2025-09-19 14:32:13 +0200

    ADDED INPUT ROUTE
    This is the addition of CRUD

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('articles', [\App\Http\Controllers\ArticleController::class, 'index'] );
 Route::get('articles/{id}', [\App\Http\Controllers\ArticleController::class, 'show']);
+Route::get('articles/create', [\App\Http\Controllers\ArticleController::class, 'create']);
+Route::post('articles/store', [\App\Http\Controllers\ArticleController::class, 'store'])->name('articles.store');
+Route::get('articles/{id}/edit', [\App\Http\Controllers\ArticleController::class, 'edit']);
+Route::put('articles/{id}/update', [\App\Http\Controllers\ArticleController::class, 'update'])->name('articles.update');
+Route::delete('articles/{id}/delete', [\App\Http\Controllers\ArticleController::class, 'destroy'])->name('articles.destroy');
 
 Route::get('authors', [AuthorController::class, 'index']);
 Route::get('authors/{id}', [AuthorController::class, 'show']);
